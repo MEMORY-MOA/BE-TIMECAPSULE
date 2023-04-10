cf6de8910e8b5459ca4f7367bc1005ccaf00938c
yejin013 <yejin013@naver.com>
2023-04-10 21:15:11 +0900

    init

---
+++ src/main/java/com/moa/timecapsule/TimecapsuleApplication.java
@@ -0,0 +1,13 @@
+package com.moa.timecapsule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecapsuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimecapsuleApplication.class, args);
+    }
+
+}
