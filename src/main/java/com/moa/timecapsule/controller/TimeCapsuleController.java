f9fee594fbbc06f23f1ed605fd617d3312d22b41
채예진 <45252618+yejin013@users.noreply.github.com>
2023-05-22 06:49:06 +0900

    [#1] 타임캡슐 URL 변경

--- src/main/java/com/moa/timecapsule/controller/TimeCapsuleController.java
+++ src/main/java/com/moa/timecapsule/controller/TimeCapsuleController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/time-capsule")
+@RequestMapping("/time-capsules")
 @RequiredArgsConstructor
 @Slf4j
 public class TimeCapsuleController {
