1980e85392630701c4899a527ce94548585f9607
ahinkim <ahin98@naver.com>
2023-07-13 10:18:51 +0900

    [#26] feat: 타임캡슐 아이템 inventory entity 생성

---
+++ src/main/java/com/moa/timecapsule/entity/ItemInventory.java
@@ -0,0 +1,30 @@
+package com.moa.timecapsule.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemInventory {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false)
+	private int itemId;
+
+	@Column(nullable = false)
+	private int itemType;
+
+	@Column(nullable = false)
+	private int item;
+}
