5a2ea12f2433db6324261752e0003c946dcdc428
ahinkim <ahin98@naver.com>
2023-07-18 10:32:50 +0900

    [#29] refactor: 두 개 이상 컬럼 unique 조건 설정 추가

--- src/main/java/com/moa/timecapsule/entity/TimeCapsuleItem.java
+++ src/main/java/com/moa/timecapsule/entity/TimeCapsuleItem.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,14 +18,23 @@ import lombok.experimental.SuperBuilder;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(
+	name = "TimeCapsuleItem",
+	uniqueConstraints = {
+		@UniqueConstraint(
+			name = "contstraintTimeCapsuleItem",
+			columnNames = {"timeCapsuleId", "itemId"}
+		)
+	}
+)
 public class TimeCapsuleItem extends BaseEntity {
 	@Id
 	@GeneratedValue
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(name = "timeCapsuleId", nullable = false)
 	private UUID timeCapsuleId;
 
-	@Column(nullable = false)
+	@Column(name = "itemId", nullable = false)
 	private int itemId;
 }
