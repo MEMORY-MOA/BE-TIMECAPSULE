9fbebbc7a4a5fd82cf073cc1ec3ec60bdb23bc44
Inyoung.R <inyoung.r@gmail.com>
2023-10-05 22:45:10 +0900

    FIX: URL Mapping Fixed

--- src/main/java/com/moa/timecapsule/controller/TimeCapsuleContentController.java
+++ src/main/java/com/moa/timecapsule/controller/TimeCapsuleContentController.java
@@ -32,7 +32,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
-@RequestMapping("/time-capsules")
+@RequestMapping("/time-capsules-content")
 @RequiredArgsConstructor
 @Slf4j
 public class TimeCapsuleContentController {
