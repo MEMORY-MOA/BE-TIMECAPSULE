6b16601af7afd21caf757b8fee5f2793338ec1e0
yejin013 <yejin013@naver.com>
2024-01-01 22:54:39 +0900

    refactor: cors 변경

--- src/main/java/com/moa/timecapsule/config/WebConfig.java
+++ src/main/java/com/moa/timecapsule/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("https://localhost:5173", "https://127.0.0.1:5173")
+			.allowedOrigins("https://localhost:5173", "https://127.0.0.1:5173", "http://localhost:5173", "http://127.0.0.1:5173")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
 			.allowCredentials(true)
 			.exposedHeaders("*");
